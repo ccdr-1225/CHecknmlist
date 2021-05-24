58c0576f8a25cbb8989f5b676637264b7baf9a73
wgs <Wang6355yu@outlook>
2021-05-24 17:21:10 +0800

    Version 0.0.2.0 add

--- cslist.py
+++ cslist.py
@@ -20,38 +20,71 @@ for temp_list in pname_list:
     name_list.append(temp_list)
 for temp_list in porgname_list:
     orgname_list.append(temp_list.strip("\n"))
+
+
 while True:
     print("1：添加已观看")
     print("2：导出已观看")
-    print("3：导出未观看")
-    print("4：退出")
+    print("3：删除已观看")
+    print("4：导出未观看")
+    print("5：添加原名单")
+    print("6：删除原名单")
+    print("#：退出")
 
-    a=int(input("操作模式"))
-
+    a=input("操作模式")
+    print(type(name_list))
     sum_num = 0
-    if a==1:
-        append_nm=input("input name:")
+    if a=="1":#add name
+        append_nm=input("input name:(back)")
         if append_nm in name_list:
             print("{} is in the name_list".format(append_nm))
+        elif append_nm not in orgname_list:
+            print("{} is not in the orgname_list".format(append_nm))
         elif append_nm=="back":
             continue
         else:
             name_list.append(append_nm)
-    elif a==2:
+    elif a=="2":#output namelist
         print(name_list)
         print("the sum is {}".format(len(name_list)))
-    elif a==3:
+    elif a=="3":#delet name
+        print(name_list)
+        delet_name=input("input name:want to delet(back)")
+        if delet_name not in name_list:
+            print("{} is not in name_list".format(delet_name))
+        elif delet_name=="back":
+            continue
+        else:
+            name_list.remove(delet_name)
+    elif a=="4":#output no list name
         for temp_name in orgname_list:
             if temp_name not in name_list:
                 print(temp_name)
                 sum_num+=1
         print("the sum is {} ".format(sum_num))
-    elif a==4:
+    elif a=="5":#add orgnamelist
+        append_nm=input("input name:(back)")
+        if append_nm in orgname_list:
+            print("{} is in the orgname_list".format(append_nm))
+        elif append_nm=="back":
+            continue
+        else:
+            orgname_list.append(append_nm)
+    elif a=="6":#delet orgnamelist
+        print(orgname_list)#
+        delet_name=input("input name:want to delet(back)")
+        if delet_name not in orgname_list:
+            print("{} is not in orgname_list".format(delet_name))
+        elif delet_name=="back":
+            continue
+        else:
+            orgname_list.remove(delet_name)
+    elif a=="#":#break
         break
-    elif a==101:
-        print(name_list)
+    elif a=="101":#debug 
+        print("name_list == {}".format(name_list))
         print(len(name_list))
-        print(orgname_list)
+        print("orgname_list =={}".format(orgname_list))
         print(len(orgname_list))
 
     name_list=list(set(name_list))
@@ -60,13 +93,14 @@ while True:
 fname_list=[]
 for temp_list in name_list:
     fname_list.append(temp_list+'\n')
+forgname_list=[]
+for temp_list in orgname_list:
+    forgname_list.append(temp_list+'\n')
 
 opnlist = open ('python\listmagnger\oprlist.txt','w')
 opnlist.writelines(fname_list)
 opnlist.close()
+opnlist = open ('python\listmagnger\orglist.txt','w')
+opnlist.writelines(forgname_list)
+opnlist.close()
 
-#更新日志 2021-5-18 Version 0.0.1.1 
-#加入导出已观看时 加入人数统计
-#修复已观看名单第一个为空的问题
-#增加错误输入回调（开发者指令——back）
-#增加数据查看入口101
