a890cb035a78ba3a640bbda7c716b97a47e97e7a
wgs <Wang6355yu@outlook>
2021-06-17 11:29:49 +0800

    Version 0.1.1.0 add init_

--- cslist.py
+++ cslist.py
@@ -5,14 +5,7 @@ import json
 #打开操作表-操作输出名单-重置
 print(os.getcwd())
 
-# 更新日志 Version 0.1.0.1 m_debug
-# 修改删除个人信息无法彻底删除
-# 增加删除前名单展示功能
-# 更改删除名单提示语
-# 增加删除确认操作
-# 增加删除名单时，总数显示
-# 修复完成名单 名字显示错误
-# 增加显示已完成人数功能
+
 
 # dic = {  
 #     'andy':{  
@@ -31,21 +24,24 @@ print(os.getcwd())
 # print(dic.get(nm))
 # print(dic.get(nm).clear())
 
-
-file_oname=open("python\listmagnger\\namelist\dit_onamelist.txt","r")
-js_oname=file_oname.read()
-#print(js_oname)
-#print(type(js_oname))
-dict_oname=json.loads(js_oname)
+try:
+    file_oname=open("namelist\dit_onamelist.txt","r+")
+    js_oname=file_oname.read()
+    #print(js_oname)
+    #print(type(js_oname))
+    dict_oname=json.loads(js_oname)
+except:
+    print("dit_onamelist.txt open failed")
 # print(type(dict_oname))
 # print(dict_oname)
 
 # print(dict_oname["andy"]["age"])
-
-file_pname=open("python\listmagnger\\namelist\dit_pnamelist.txt","r")
-js_pname=file_pname.read()
-dict_pname=json.loads(js_pname)
-
+try:
+    file_pname=open("namelist\dit_pnamelist.txt","r+")
+    js_pname=file_pname.read()
+    dict_pname=json.loads(js_pname)
+except:
+    print("dit_pnamelist.txt open failed")
 # porgnamelist = open ('python\\listmagnger\\namelist\\orglist.txt','r')
 # porgname_list=porgnamelist.readlines()
 # porgnamelist.close()
@@ -63,147 +59,161 @@ dict_pname=json.loads(js_pname)
 # for temp_list in porgname_list:
 #     orgname_list.append(temp_list.strip("\n"))
 
-
-while True:
-    tempinput=input("input:")
-    print(type(dict_oname),type(dict_pname))
-    if tempinput=="help":
-        print("o-add-name o-add-id o-add-tips")
-        print("o-output-all ")
-        print("o-delet-all o-delet-name")
-        print("o-modify")
-        print("p-add-name")
-        print("p-output-all p-output-name")
-        print("p-delet-all p-delet-name")
-        print("output-all")
-        print("exit")
-    elif tempinput=="exit":
-        break
-    elif tempinput=="o-add-name":
-        while True:
-            name_input=input("input name:")
-            if name_input =="back":
-                break
-            elif name_input in dict_oname:
-                print("{} is in the dict_oname")
-            else:
-                dict_oname.setdefault(name_input,{})
-    elif tempinput=="o-add-id":
-        while True:
-            name_input=input("name first:")
-            if name_input=="back":
-                break
-            elif name_input in dict_oname:
-                print("This name is in the orgname")
-                print(dict_oname.get(name_input))
-                id_input=input("input id:")
-                if id_input=='back':
+try:
+    while True:
+        tempinput=input("input:")
+        print(type(dict_oname),type(dict_pname))
+        if tempinput=="help":
+            print("o-add-name o-add-id o-add-tips")
+            print("o-output-all ")
+            print("o-delet-all o-delet-name")
+            print("o-modify")
+            print("p-add-name")
+            print("p-output-all p-output-name")
+            print("p-delet-all p-delet-name")
+            print("output-all")
+            print("output\n\toutput no finish name list")
+            print("init_\n\tinit onamelist")
+            print("exit")
+        elif tempinput=="exit":
+            break
+        elif tempinput=="o-add-name":
+            while True:
+                name_input=input("input name:")
+                if name_input =="back":
                     break
+                elif name_input in dict_oname:
+                    print("{} is in the dict_oname")
                 else:
-                    dict_oname.get(name_input)['id']=id_input
-            else:
-                print("This name is not in the orgname")
-    elif tempinput=="o-add-tips":
-        print("tips is not developed")
-    elif tempinput=="o-output-all":
-        print(dict_oname)
-        print(len(dict_oname))
-    elif tempinput=='o-delet-all':
-        dict_oname.clear()
-        print(dict_oname)
-        print("dict_oname is clear")
-    elif tempinput=="o-delet-name":
-        while True:
-            print(dict_oname)
-            print(len(dict_oname),end="\n====**===")
-            name_input=input("input name:(you want to delet)")
-            if name_input=="back":
-                break
-            elif name_input not in dict_oname:
-                print("{} is not in dict_oname".format(name_input))
-            else:
-                besure=input("are you sure to delet {}:(y/...)".format(name_input))
-                if besure=="y" or besure=="Y":
-                    dict_oname.pop(name_input)
-                    print("{} delete finish".format(name_input))
-                else:
-                    print("{} delete fail".format(dict_oname))
-    elif tempinput=="o-modify":
-        while True:
-            print(dict_oname)
-            name_input=input('name first')
-            if name_input=='back':
-                break
-            elif name_input not in dict_oname:
-                print("{} is not in dict_oname".format(name_input))
-            else :
-                while True:
+                    dict_oname.setdefault(name_input,{})
+        elif tempinput=="o-add-id":
+            while True:
+                name_input=input("name first:")
+                if name_input=="back":
+                    break
+                elif name_input in dict_oname:
+                    print("This name is in the orgname")
                     print(dict_oname.get(name_input))
-                    # modify_dict=dict_oname.get(name_input)
-                    tempinput=input("What you want to modify:")
-                    if tempinput == "back":
+                    id_input=input("input id:")
+                    if id_input=='back':
                         break
-                    elif tempinput == "name":
-                        new_name_input=input("new name:")
-                        if new_name_input in dict_oname:
-                            print("the new name '{}' is in the dict_oname".format(new_name_input))
-                        else:
-                            dict_oname[new_name_input]=dict_oname.pop(name_input)
-                            print("name modify finish")
-                    elif tempinput=="id":
-                        new_id_input=input("new id:")
-                        dict_oname[name_input]["id"]=new_id_input
-                        print("id modify finish")
-                    elif tempinput=="tips":
-                        new_tips_input=input("new tips:")
-                        dict_oname[name_input]["tips"]=new_tips_input
-                        print("tips modify finish")
-                    else :
-                        print("input error")
-    elif tempinput=="p-add-name":
-        while True:
-            add_name_input=input("finish name:")
-            if add_name_input=="back":
-                break
-            elif add_name_input in dict_pname:
-                print("{} is in the dict_pname")
-            elif add_name_input not in dict_oname:
-                print("{} is not in the dict_oname ,name error")
-            else:
-                temp_dict=dict_oname[add_name_input]
-                dict_pname[add_name_input]=temp_dict
-                print("{} add_pname finish".format(add_name_input))
-                # print(dict_pname[add_name_input])
-    elif tempinput=="p-output-all":
-        print(dict_pname)
-    elif tempinput=="p-output-name":
-        print(dict_pname.keys())
-        print(len(dict_pname.keys()))
-    elif tempinput=="p-delet-all":
-        dict_pname.clear()
-        print("dict_pname clear finish")
-    elif tempinput=="p-delet-name":
-        while True:
-            print(dict_pname)
-            print(len(dict_pname),end="\n")
-            name_input=input("input name(you want to delet):")
-            if name_input=="back":
-                break
-            elif name_input not in dict_pname:
-                print("{} is not in dict_pname".format(name_input))
-            else:
-                besure=input("are you sure to delet {}:(y/...)".format(name_input))
-                if besure=="y" or besure=="Y":
-                    dict_pname.pop(name_input)
-                    print("{} delete finish".format(name_input))
+                    else:
+                        dict_oname.get(name_input)['id']=id_input
                 else:
-                    print("{} delete fail".format(dict_pname))
-    elif tempinput=="output-all":
-        print(dict_oname)
-        print(dict_pname)
-    else :
-        print("fun error:101")
-
+                    print("This name is not in the orgname")
+        elif tempinput=="o-add-tips":
+            print("tips is not developed")
+        elif tempinput=="o-output-all":
+            print(dict_oname)
+            print(len(dict_oname))
+        elif tempinput=='o-delet-all':
+            dict_oname.clear()
+            print(dict_oname)
+            print("dict_oname is clear")
+        elif tempinput=="o-delet-name":
+            while True:
+                print(dict_oname)
+                print(len(dict_oname),end="\n====**===")
+                name_input=input("input name:(you want to delet)")
+                if name_input=="back":
+                    break
+                elif name_input not in dict_oname:
+                    print("{} is not in dict_oname".format(name_input))
+                else:
+                    besure=input("are you sure to delet {}:(y/...)".format(name_input))
+                    if besure=="y" or besure=="Y":
+                        dict_oname.pop(name_input)
+                        print("{} delete finish".format(name_input))
+                    else:
+                        print("{} delete fail".format(dict_oname))
+        elif tempinput=="o-modify":
+            while True:
+                print(dict_oname)
+                name_input=input('name first')
+                if name_input=='back':
+                    break
+                elif name_input not in dict_oname:
+                    print("{} is not in dict_oname".format(name_input))
+                else :
+                    while True:
+                        print(dict_oname.get(name_input))
+                        # modify_dict=dict_oname.get(name_input)
+                        tempinput=input("What you want to modify:")
+                        if tempinput == "back":
+                            break
+                        elif tempinput == "name":
+                            new_name_input=input("new name:")
+                            if new_name_input in dict_oname:
+                                print("the new name '{}' is in the dict_oname".format(new_name_input))
+                            else:
+                                dict_oname[new_name_input]=dict_oname.pop(name_input)
+                                print("name modify finish")
+                        elif tempinput=="id":
+                            new_id_input=input("new id:")
+                            dict_oname[name_input]["id"]=new_id_input
+                            print("id modify finish")
+                        elif tempinput=="tips":
+                            new_tips_input=input("new tips:")
+                            dict_oname[name_input]["tips"]=new_tips_input
+                            print("tips modify finish")
+                        else :
+                            print("input error")
+        elif tempinput=="p-add-name":
+            while True:
+                add_name_input=input("finish name:")
+                if add_name_input=="back":
+                    break
+                elif add_name_input in dict_pname:
+                    print("{} is in the dict_pname".format(add_name_input))
+                elif add_name_input not in dict_oname:
+                    print("{} is not in the dict_oname ,name error".format(add_name_input))
+                else:
+                    temp_dict=dict_oname[add_name_input]
+                    dict_pname[add_name_input]=temp_dict
+                    print("{} add_pname finish".format(add_name_input))
+                    # print(dict_pname[add_name_input])
+        elif tempinput=="p-output-all":
+            print(dict_pname)
+        elif tempinput=="p-output-name":
+            print(dict_pname.keys())
+            print(len(dict_pname.keys()))
+        elif tempinput=="p-delet-all":
+            dict_pname.clear()
+            print("dict_pname clear finish")
+        elif tempinput=="p-delet-name":
+            while True:
+                print(dict_pname)
+                print(len(dict_pname),end="\n")
+                name_input=input("input name(you want to delet):")
+                if name_input=="back":
+                    break
+                elif name_input not in dict_pname:
+                    print("{} is not in dict_pname".format(name_input))
+                else:
+                    besure=input("are you sure to delet {}:(y/...)".format(name_input))
+                    if besure=="y" or besure=="Y":
+                        dict_pname.pop(name_input)
+                        print("{} delete finish".format(name_input))
+                    else:
+                        print("{} delete fail".format(dict_pname))
+        elif tempinput=="output-all":
+            print("onamelist:")
+            print(dict_oname)
+            print(len(dict_oname))
+            print("pnamelist:")
+            print(dict_pname)
+            print(len(dict_pname))
+        elif tempinput=="output":
+            for name in dict_oname:
+                if name not in dict_pname:
+                    print(name,end=" ")
+            print()
+        elif tempinput=="init_":
+            dict_oname={"\u5468\u6590\u7136": {}, "\u7530\u96e8\u6674": {}, "\u9b4f\u8513\u6e1d": {}, "\u738b\u82cf\u7136": {}, "\u5f20\u6676\u6676": {}, "\u4f5f\u8d5b\u6960": {}, "\u8c37\u6587\u6587": {}, "\u539f\u5b5d\u6dfb": {}, "\u5b59\u6587\u793c": {}, "\u59dc\u6d77\u946b": {}, "\u8521\u666f\u8d35": {}, "\u59da\u5929\u5b87": {}, "\u5f20\u660e\u8fea": {}, "\u5218\u53cc\u777f": {}, "\u4e8e\u4eac\u6dde": {}, "\u6bb5\u9701\u822a": {}, "\u9648\u4f73\u8302": {}, "\u8d56\u6b63\u8f89": {}, "\u674e\u52c3\u7136": {}, "\u738b\u70ab\u6d66": {}, "\u5f20\u8d75\u667a\u5eb7": {}, "\u738b\u6668": {}, "\u9ea6\u9f99\u6cc9": {}, "\u8bb8\u4f73\u5ef7": {}, "\u90d1\u94b0\u5c71": {}, "\u674e\u9e4f": {}, "\u4e25\u5cf0": {}, "\u66f9\u6c38\u6770": {}, "\u674e\u6069\u6cfd": {}, "\u5173\u6dde\u6dcb": {}, "\u5510\u7389\u5c27": {}, "\u738b\u701a\u5174": {}, "\u6155\u5347\u5347": {}, "\u5c39\u5723\u6dfc": {}, "\u738b\u6a31\u6d01": {}, "\u674e\u5fe0\u667a": {}, "\u9648\u76c8\u8bfa": {}, "\u5f90\u4e1c": {}, "\u5415\u601d\u8fbe": {}, "\u738b\u51a0\u8212": {}}
+        else :
+            print("fun error:101")
+except:
+    print("main run failed")
 
 #     a=input("操作模式")
 #     print(type(name_list))
@@ -282,13 +292,17 @@ while True:
 # opnlist.writelines(forgname_list)
 # opnlist.close()
 
-
-js = json.dumps(dict_oname)
-file = open('python\listmagnger\\namelist\dit_onamelist.txt', 'w')  
-file.write(js)
-file.close()
-js = json.dumps(dict_pname)
-file = open('python\listmagnger\\namelist\dit_pnamelist.txt', 'w')  
-file.write(js)
-file.close()
-
+try:
+    js = json.dumps(dict_oname)
+    file = open('namelist\dit_onamelist.txt', 'w+')  
+    file.write(js)
+    file.close()
+except:
+    print("write onamelist.txt failed")
+try:
+    js = json.dumps(dict_pname)
+    file = open('namelist\dit_pnamelist.txt', 'w+')  
+    file.write(js)
+    file.close()
+except:
+    print("write pnamelist.txt failed")
